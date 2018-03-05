637f6cb0ba7bdbf4cf67d5db5935d6d1b742fb3d
MantasVitkunas <mantas.vitkunas@gmail.com>
2018-03-05 14:18:38 +0200

    Reference fixes

--- Executor/Program.cs
+++ Executor/Program.cs
@@ -6,7 +6,8 @@ namespace Executor
     {
         static void Main(string[] args)
         {
-            new ObjectGenerator.Generator().GenerateObjects("C:\\Users\\manta\\Documents\\Visual Studio 2017\\Projects\\ObjectGenerator\\ObjectGenerator\\");
+            //new ObjectGenerator.Generator().GenerateObjects("C:\\Users\\manta\\Documents\\Visual Studio 2017\\Projects\\ObjectGenerator\\ObjectGenerator\\");
+            new ObjectGenerator.Generator().GenerateObjects("Z:\\Projects\\git\\ObjectGenerator\\ObjectGenerator");
             Console.ReadKey();
         }
     }
--- ObjectGenerator/Generator.cs
+++ ObjectGenerator/Generator.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using ObjectGenerator.Models;
 using System.IO;
 using RazorEngine;
